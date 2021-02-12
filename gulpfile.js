c9c2a56ebba745dd3f04c16d822e75fea6e54f9a
pdkpavankumar <pavankumar8545@gmail.com>
2021-02-12 20:00:48 +0530

    intial config commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const { series, dest } = require("gulp");
+const fs = require('fs');
+const del = require('del');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+
+const SRC_PATH = './src';
+const DEST_PATH = './dest';
+
+function clean(cb) {
+  if (fs.existsSync(DEST_PATH)) {
+    del.sync([DEST_PATH]);
+  }
+  cb();
+}
+
+function create(cb) {
+  if (!fs.existsSync(DEST_PATH)) {
+    fs.mkdirSync(DEST_PATH);
+  }
+  cb();
+}
+
+function copy(cb) {
+  var files = fs.readdirSync(SRC_PATH);
+  files.forEach(file => {
+    var cssData = fs.readFileSync(SRC_PATH + '/' + file);
+    postcss([autoprefixer]).process(cssData, { from: SRC_PATH + '/' + file, to: DEST_PATH + '/' + 'wind-ui.css' }).then(result => {
+      fs.appendFile('dest/wind-ui.css', result.css + '\n\n', () => true);
+      if (result.map) {
+        fs.writeFile('dest/app.css.map', result.map.toString(), () => true)
+      }
+    });
+  });
+  cb();
+}
+
+exports.default = series(clean, create, copy);
